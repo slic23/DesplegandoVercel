fca508f344901c081e2ab109ba6ecba0c45d40cf
Issam Ramdani <socrates98@protonmail.com>
2025-02-06 20:10:16 +0100

    script start

--- start.sh
+++ start.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-echo "Instalando requerimientos..."
-pip3 install -r requirements.txt
-
-echo "Iniciando migraciones..."
-python3 manage.py makemigrations && python3 manage.py migrate
-
-echo "Colectando StaticFiles..."
-python3 manage.py collectstatic --noinput
-
-echo "Iniciando servidor..."
-gunicorn myproject.wsgi:application --bind 0.0.0.0:$PORT
+pip install gunicorn
+gunicorn locallibrary.wsgi:application --bind 0.0.0.0:8000
