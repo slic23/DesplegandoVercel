50be7d4f2ebfbcd99cb12e4472cfc860e899580b
Issam Ramdani <73023437+slic23@users.noreply.github.com>
2025-02-06 22:33:13 +0100

    Update migrate_modelDB.sh

--- migrate_modelDB.sh
+++ migrate_modelDB.sh
@@ -8,5 +8,4 @@ python3 manage.py makemigrations && python3 manage.py migrate
 # Colecta de staticsFiles en theme/static/
 echo "Colectando StaticFiles..."
 python3 manage.py collectstatic --noinput
-echo "Iniciando servidor..."
-python3 manage.py runserver 0.0.0.0:$PORT 
+
